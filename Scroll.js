bf4d3a6f5a32c6ea96974609141e71c215c553af
nguyenanh02k1 <nguyenanh02k1@gmail.com>
2024-01-15 14:55:46 +0700

    Huy

--- Scroll.js
+++
@@ -1,4 +0,0 @@
-if ($(document).height() > $(window).height()) {
-    var scrollTop = ($('html').scrollTop()) ? $('html').scrollTop() : $('body').scrollTop(); // Works for Chrome, Firefox, IE...
-    $('html').addClass('noscroll').css('top',-scrollTop);         
-}
